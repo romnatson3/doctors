7310621ce2680e384cf9bdeb67bf09658e332e0d
Roman <roman@gmail.com>
2023-11-12 23:07:07 +0200

    Fix address, phone

--- bot/forms.py
+++ bot/forms.py
@@ -23,31 +23,21 @@ class SpecialityForm(forms.ModelForm):
 class PolyclinicForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        instance = kwargs.get('instance')
-        queryset = Phone.objects.filter(polyclinic=instance).all()
-        self.fields['phone'].queryset = queryset
-        queryset = Address.objects.filter(polyclinic=instance).all()
-        self.fields['address'].queryset = queryset
-        self.fields['speciality'].widget.can_add_related = False
+        self.fields['district'].required = True
+        # instance = kwargs.get('instance')
+        # queryset = Address.objects.filter(polyclinic=instance).all()
+        # self.fields['address'].queryset = queryset
+        # self.fields['speciality'].widget.can_add_related = False
 
     class Meta:
         model = Polyclinic
         fields = '__all__'
 
 
-class ScheduleForm(forms.ModelForm):
+class DoctorForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        instance = kwargs.get('instance')
-        queryset = Address.objects.filter(polyclinic=instance.polyclinic).all()
-        self.fields['address'].queryset = queryset
 
-    class Meta:
-        model = Schedule
-        fields = '__all__'
-
-
-class DoctorForm(forms.ModelForm):
     schedule = forms.ModelMultipleChoiceField(
         queryset=Schedule.objects.all(),
         required=True,
