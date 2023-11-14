741a81af1345b6ce721640c372f696e05a80382e
Roman <romnatson3@gmail.com>
2023-11-14 09:01:45 +0200

    Redid rating

--- bot/views.py
+++ bot/views.py
@@ -33,7 +33,7 @@ def telegram_webhook(request):
                     logging.info(f'Create new user: {user.id} {user.username} {user.first_name} {user.last_name}')
                 send_message_to_new_user.delay(message.from_user.id)
             elif message.text == 'Мой доктор':
-                send_message_clinic_or_private.delay(message.from_user.id)
+                send_message_specialities.delay(message.from_user.id)
 
         if body.callback_query:
             message = body.callback_query
@@ -46,10 +46,10 @@ def telegram_webhook(request):
                 return HttpResponse(status=200)
 
             if data.get('type') == 'speciality':
-                send_message_districts.delay(message.from_user.id, message.message.message_id, data['data'])
+                send_message_clinic_or_private.delay(message.from_user.id, message.message.message_id, data['data'])
 
             if data.get('type') == 'clinic_or_private':
-                send_message_specialities.delay(message.from_user.id, message.message.message_id, data['data'])
+                send_message_districts.delay(message.from_user.id, message.message.message_id, data['data'])
 
             if data.get('type') == 'district':
                 clinic_or_private, speciality_id, district_id = data['data'].split(',')
@@ -57,38 +57,31 @@ def telegram_webhook(request):
                 district_id = int(district_id)
 
                 if clinic_or_private == 'private':
-                    doctors_id = []
+                    result = []
                     doctors = Doctor.objects.prefetch_related('district').filter(speciality=speciality_id).all()
                     if doctors:
                         for doctor in doctors:
                             if list(filter(lambda x: x == district_id, [i.id for i in doctor.district.all()])):
-                                doctors_id.append(doctor.id)
+                                result.append(doctor)
 
-                    if doctors_id:
-                        speciality = (Speciality.objects.select_related(
-                            'rating_1', 'rating_2', 'rating_3', 'rating_4', 'rating_5')
-                            .filter(id=speciality_id).first())
-                        doctors_rating = [
-                            speciality.rating_5, speciality.rating_4, speciality.rating_3,
-                            speciality.rating_2, speciality.rating_1
-                        ]
-                        for doctor in doctors_rating:
-                            if doctor and doctor.id in doctors_id:
-                                doctors_id.remove(doctor.id)
-                                doctors_id.insert(0, doctor.id)
+                    if result:
+                        result = sorted(result, key=lambda x: int(x.rating) if x.rating else 10)
+                        doctors_id = [i.id for i in result]
                         send_message_doctor.delay(message.from_user.id, message.message.message_id, doctors_id)
                     else:
                         send_message('sendMessage', chat_id=message.from_user.id, parse_mode='HTML', text=f'<i>{texts.no_doctors}</i>')
 
                 elif clinic_or_private == 'clinic':
-                    polyclinics_id = []
+                    result = []
                     polyclinics = Polyclinic.objects.prefetch_related('speciality').filter(district__id=district_id).all()
                     if polyclinics:
                         for polyclinic in polyclinics:
                             if list(filter(lambda x: x == speciality_id, [i.id for i in polyclinic.speciality.all()])):
-                                polyclinics_id.append(polyclinic.id)
+                                result.append(polyclinic)
 
-                    if polyclinics_id:
+                    if result:
+                        result = sorted(result, key=lambda x: int(x.rating) if x.rating else 10)
+                        polyclinics_id = [i.id for i in result]
                         send_message_polyclinic.delay(message.from_user.id, message.message.message_id, polyclinics_id)
                     else:
                         send_message('sendMessage', chat_id=message.from_user.id, parse_mode='HTML', text=f'<i>{texts.no_doctors}</i>')
